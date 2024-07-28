5b7d945b397f25f721a3dc94a2672ce8cc9da25d
renan.rodrigues.glo <renan.rodrigues.3@globalhitss.com.br>
2024-07-27 21:51:33 -0300

    In progress

--- scripts.js
+++ scripts.js
@@ -1,8 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const menuIcon = document.getElementById('menu-icon');
 	const menu = document.getElementById('menu');
+	const menuBackground = document.createElement('div');
+	menuBackground.id = 'menu-background';
+
+	document.body.appendChild(menuBackground);
 
 	menuIcon.addEventListener('click', function() {
 		menu.classList.toggle('open');
+		menuBackground.classList.toggle('visible');
+	});
+
+	menuBackground.addEventListener('click', function() {
+		menu.classList.remove('open');
+		menuBackground.classList.remove('visible');
+	});
+
+	document.addEventListener('keydown', function(event) {
+		if (event.key === 'Escape') {
+			menu.classList.remove('open');
+			menuBackground.classList.remove('visible');
+		}
 	});
 });
